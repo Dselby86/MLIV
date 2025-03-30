2cec49b46338aa85c6711020b8d00b5022809f1c
David Selby <Dselby86@gmail.com>
2025-03-30 14:21:21 -0700

    Add files via upload

---
+++ treatment_effects/10-call_sim_w_asap.R
@@ -0,0 +1,149 @@
+###################################################################################
+#                                                                                                        
+#                                                                         
+# Created on:   07/02/2024
+# Purpose:      Template for calling the simulation with ASAP data
+# Authors:      Luke Miratrix, Polina Polskaia, Nick Commins
+# 
+#
+###################################################################################
+
+
+###################################################################################
+# STEP 0: SET UP 
+###################################################################################
+
+
+# Load script 08
+source(here::here("simulation_pipeline/08_run_simulation.R"))
+
+###################################################################################
+# STEP 1: CLEAN ASAP DATA
+###################################################################################
+
+
+# Load CA data
+asap_subset_real <- readRDS("/data/share/cdi/MLIV/Data/Real/asap_subset.rds")
+
+# Create covariate sets
+asap_small_set <- c(
+  # Small set 
+  "BLRAET1" ,        # Race/ethnicity
+  "BLAGENUM",        # Age
+  "BLFEMALE",         # Gender (Female dummy)
+  "BLWORK",          # Currently employed
+  "BLCHLD",          # Has children
+  "BLP50"           # Parents pay more than 50% of educational expenses
+)
+
+asap_medium_set <- c(
+  # Small set 
+  "BLRAET1" ,        # Race/ethnicity
+  "BLAGENUM",        # Age
+  "BLFEMALE",        # Gender (Female dummy)
+  "BLWORK",          # Currently employed
+  "BLCHLD",          # Has children
+  "BLP50",           # Parents pay more than 50% of educational expenses
+  
+  # Medium set
+  "BLC1ST",          # First in immediate family to attend college
+  "BLLIV",           # Marital status and living situation
+  "BLDEGS_HSDP"      # Diplomas and degrees earned
+)
+
+asap_large_set <- c(
+  # Small set 
+  "BLRAET1" ,        # Race/ethnicity
+  "BLAGENUM",        # Age
+  "BLFEMALE",        # Gender (Female dummy)
+  
+  # Medium set
+  "BLWORK",          # Currently employed
+  "BLCHLD",          # Has children
+  "BLP50",           # Parents pay more than 50% of educational expenses
+  "BLC1ST",          # First in immediate family to attend college
+  "BLLIV",           # Marital status and living situation
+  "BLDEGS_HSDP",     # Diplomas and degrees earned
+  
+  # Large set
+  "BLDPDT",          # Date earned high school diploma
+  "BLHSGR"  ,        # Highest grade completed
+  "BLLANG",          # Speak a language other than English at home
+  "BLDGPL2",         # Highest Degree Planned
+  "BLCNUM",          # Number of children
+  "BLCAGENUM",       # Age of youngest child - converted  from categorical to numeric 
+  "BLDEGS_GED",      # Diplomas and degrees earned
+  "BLDEGS_TECH",     # Diplomas and degrees earned
+  "BLDEGS_NONE"      # Diplomas and degrees earned - not running currently, still leaving at the end for now 
+  
+)
+
+# Specify treatment variable
+asap_treatment <- "STRA_CODE"
+
+# Specify outcomes:
+asap_outcomes <- c(
+  "C16BMVDEG",       # Earned a degree from any college (%) (CUNY + OHIO)
+  "X16BTMCRET",      # Total credits earned (CUNY) ("X"-prefix because excluding repeated courses)
+  "SessionsEnrolled",# Sessions enrolled (out of 12) (CUNY)  #NC: this is currently out of 17, not 12
+  "AnyEnrollment" 
+)
+
+###################################################################################
+# STEP 2: SUBSET TO ONLY VARIABLES WE ARE INTERESTED IN
+###################################################################################
+
+
+# Subset to covariates,treatment, and outcomes 
+asap_subset_real <- asap_subset_real[ , c(asap_outcomes, asap_large_set, asap_treatment)]
+
+###################################################################################
+# STEP 3: IMPUTE COVARIATES
+###################################################################################
+
+
+# IMPUTATION STRATEGY
+# For continuous variables:  impute with mean
+# For categorical variables: add a level for missing
+# For dummy variables: 	     impute the mean
+
+asap_continuous <- c("BLAGENUM", "BLCAGENUM", "BLCNUM", "BLDPDT") 
+
+asap_categorical <- c("BLRAET1", "BLLIV", "BLDGPL2", "BLLANG") 
+
+asap_dummy <- c("BLFEMALE", "BLCHLD", "BLWORK", "BLP50", "BLC1ST",
+                "BLDEGS_HSDP", "BLDEGS_GED", "BLDEGS_TECH", "BLDEGS_NONE",
+                "BLHSGR") 
+
+# Imputing continuous variables with mean
+asap_subset_imputed <- asap_subset_real
+asap_subset_imputed[ , asap_continuous] <- lapply(asap_subset_real[,asap_continuous], function(x)
+  ifelse(is.na(x), floor(mean(x, na.rm = TRUE)), x)
+)
+
+# Impute categorical vars with 99
+asap_subset_imputed[ , asap_categorical] <- lapply(asap_subset_imputed[,asap_categorical], function(x)
+  ifelse(is.na(x), 99, x)
+)
+
+# Impute dummies with the mean
+asap_subset_imputed[ , asap_dummy] <- lapply(asap_subset_imputed[,asap_dummy], function(x)
+  ifelse(is.na(x), mean(x, na.rm = TRUE), x)
+)
+
+# Sample size check
+# N cols and rows should be the same
+stopifnot(dim(asap_subset_real) == dim(asap_subset_imputed))
+
+###################################################################################
+# STEP 4: CONVERT CATEGORICAL VARIABLES TO FACTORS
+###################################################################################
+
+
+# Convert categorical vars to factors
+asap_subset_imputed$BLRAET1 <- as.factor(asap_subset_imputed$BLRAET1)
+asap_subset_imputed$BLLIV <- as.factor(asap_subset_imputed$BLLIV)
+asap_subset_imputed$BLHSGR <- as.factor(asap_subset_imputed$BLHSGR)
+asap_subset_imputed$BLLANG <- as.factor(asap_subset_imputed$BLLANG)
+asap_subset_imputed$BLCNUM <- as.factor(asap_subset_imputed$BLCNUM)
+asap_subset_imputed$BLDGPL2 <- as.factor(asap_subset_imputed$BLDGPL2)
---
+++ treatment_effects/21-Calculate CATES_ca.R
@@ -0,0 +1,172 @@
+
+
+#############################################
+#
+# Step 0: Setup Code
+#
+############################################
+library(here)
+library(tidyverse)
+library(rlang)
+library(ggExtra)
+library(kableExtra)
+library(knitr)
+library(rsample)
+
+# Styler
+library(styler)
+library(lintr)
+
+# Load previous programs
+source(here::here("R/packages.R"))
+source(here::here("algorithms/cf_alg.R"))
+source(here::here("treatment_effects/20-call_sim_w_ca.R")) # Most Recent Simulation
+#list.files(here::here("treatment_effects"))
+
+########################################
+#
+# Step 1: File paths
+#@NOTE: change for File
+#######################################
+dat_raw <- ca_subset_imputed
+treatment <- ca_treatment
+y_binary <- "BMEFTMEDIAN"
+y_cont <- "Y18JBERNA_06"
+cv <- ca_large_set
+factor_vars <- c("ETHNIC", "READCAT", "MATHCAT", "PARENT", "edfath", "edmoth", "HRSTV", "HRSHW", "PARWORK", "PSEDEXP", "MOVED", "OFFICE")
+cv <-cv[!(cv %in% factor_vars)]
+
+
+########################################
+#
+#Step 2: Prepare Data set
+#
+#########################################
+
+dat <- dat_raw %>%
+  select( all_of (c(y_binary, y_cont, treatment, cv))) %>%
+  filter(!is.na(!!sym(y_binary )),
+         !is.na(!!sym(y_cont ))
+  )
+
+# Set row names for id's
+dat$id <- 1:nrow(dat)
+
+# Convert To Dummy Variables
+dat
+
+
+######
+
+
+df <- dat
+
+
+# Function to perform a split and Calculate CATES from each output
+predict_cate_cf <- function(df) {
+  # Split dataset into train/test split
+  split <- initial_split(df, prop = 0.5)
+  
+  df_1 <- training(split)
+  df_2 <- testing(split)
+  
+  # Create objects for Causal forest off Dataframe 1.
+  y_cont_1 <- df_1 %>%
+    select(all_of(y_cont)) %>%
+    as.matrix()
+  
+  y_binary_1 <- df_1 %>%
+    select(all_of(y_binary)) %>%
+    as.matrix()
+  
+  t_1 <- df_1 %>%
+    select(all_of(treatment)) %>%
+    as.matrix()
+  
+  cv_1 <- df_1 %>%
+    select(all_of(cv)) %>%
+    as.matrix()
+  
+  id_1 <- df_1 %>%
+    select(id)
+  
+  # Create Vectors for Causal forest off Dataframe 1.
+  y_cont_2 <- df_2 %>%
+    select(all_of(y_cont)) %>%
+    as.matrix()
+  
+  y_binary_2 <- df_2 %>%
+    select(all_of(y_binary)) %>%
+    as.matrix()
+  
+  t_2 <- df_2 %>%
+    select(all_of(treatment)) %>%
+    as.matrix()
+  
+  cv_2 <- df_2 %>%
+    select(all_of(cv)) %>%
+    as.matrix()
+  
+  id_2 <- df_2 %>%
+    select(id)
+  
+  # Set Causal Forest models for Continous and Binary outcomes
+  fit_1_cont <- causal_forest(cv_1, y_cont_1, t_1, Y.hat = NULL, W.hat = NULL)
+  fit_1_binary <- causal_forest(cv_1, y_binary_1, t_1, Y.hat = NULL, W.hat = NULL)
+  
+  fit_2_cont <- causal_forest(cv_2, y_cont_2, t_2, Y.hat = NULL, W.hat = NULL)
+  fit_2_binary <- causal_forest(cv_2, y_binary_2, t_2, Y.hat = NULL, W.hat = NULL)
+  
+  # Predict Yhat for cf_1 using cf_2 model
+  cate_cont_1 <- predict(fit_2_cont, cv_1)$predictions
+  cate_binary_1 <- predict(fit_2_binary, cv_1)$predictions
+  
+  # Predict Yhat for cf_2 using cf_1 model
+  cate_cont_2 <- predict(fit_2_cont, cv_2)$predictions
+  cate_binary_2 <- predict(fit_2_binary, cv_2)$predictions
+  
+  # Build yhat dataset
+  cate_1 <- cbind(id_1, cate_cont_1, cate_binary_1)
+  cate_2 <- cbind(id_2, cate_cont_2, cate_binary_2)
+  
+  colnames(cate_1) <- c("id", "cate_cont", "cate_binary")
+  colnames(cate_2) <- c("id", "cate_cont", "cate_binary")
+  
+  cate <- rbind(cate_1, cate_2)
+  
+  return(cate)
+}
+
+#####################################
+#
+# STEP 2: FOR Loop to Calculate CATES
+# Use predict_cate_cf Function
+#####################################
+
+## NOTE: Data is organized as long dataset, and should be grouped on ID.
+
+cate_i <- predict_cate_cf(dat)
+cate_i$run <- 1
+
+for (i in 2:10) {
+  temp_cate <- predict_cate_cf(dat)
+  temp_cate$run <- i
+  cate_i <- rbind(cate_i, temp_cate)
+}
+
+
+cate_agg <- cate_i %>%
+  group_by(id) %>%
+  summarise(cate_cont = median(cate_cont),
+            cate_binary = median(cate_binary)
+  )
+
+
+##########################
+#
+# STEP 3: Rebuild Dataset
+#
+##########################
+
+dat_ca <- merge(dat, cate_agg, by = "id")
+
